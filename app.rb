c22ef389101a1a8c9c948a1913c43a1e501b448e
Oliver <oliver.wisler@endress.com>
2024-07-03 10:19:30 +0200

    feat(db): Introduce a poor mans database

--- app.rb
+++ app.rb
@@ -4,26 +4,41 @@ require 'dotenv/load'
 class App < Sinatra::Base
 
   get '/' do
-    'Hello world!'
+    content_type :json
+    File.read('database.json') rescue {}
   end
 
   post '/webhook' do
-
     # Check if the request is signed and the signature is correct
     request.body.rewind
-    puts request.env
     hash = request.env['HTTP_X_HUB_SIGNATURE_SHA256']
-    puts "Request Hash: #{hash}"
     halt 403 if hash.nil?
     calculated_signature = "sha256=#{OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), ENV['WEBHOOK_SECRET'], request.body.read)}"
-    puts "Calculated Hash: #{calculated_signature}"
+
+    if ENV['VERBOSE'] == 'true'
+      puts "Calculated signature: #{calculated_signature}"
+      puts "Received signature: #{hash}"
+    end
+
     halt 403 unless Rack::Utils.secure_compare(calculated_signature, hash)
 
-    # Parse the event
+    # Parse the event (only continue if it is the event we expect
     request.body.rewind
     event = JSON.parse request.body.read
+    halt 406 unless event['event_type'] == 'asset_value_created'
 
-    # Save it to the json we use as database ;)
+    # Save it to the json we use as database ;), this is not a good practice, but for this example it is ok
+    asset_id = event['content']['asset']['id']
+    event_parsed = {
+      time_sent: event['occurred_at'],
+      time_value: event['content']['value']['timestamp'],
+      value: event['content']['value']['value']
+    }
+
+    database = JSON.parse(File.read('database.json')) rescue {}
+    database[asset_id] = event_parsed
+    File.write('database.json', JSON.pretty_generate(database))
+
     200
   end
 end
--- app_spec.rb
+++ app_spec.rb
@@ -2,6 +2,8 @@ require "app"
 require "rack/test"
 
 ENV['RACK_ENV'] = 'test'
+ENV['VERBOSE'] = 'true'
+ENV['WEBHOOK_SECRET'] = '4q72JHgX89z3BkFMt6cwQxL1rD28jpN5UfVhIZYPbCSeuGovRaWmA0sD9ECtX7Jf'
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods
@@ -18,15 +20,73 @@ describe App do
     end
   end
 
-  context "POST to /webhook" do
+  context "Single POST to /webhook" do
+    before(:context) do
+      File.delete('database.json') if File.exist?('database.json')
+    end
+
     let(:response) {
       post "/webhook",
            { event_type: "asset_value_created", occurred_at: "2020-03-04T09:15:00", content: { asset: { id: 4079332, href: "https://api.netilion.endress.com/v1/assets/4079332" }, value: { key: "level", group: "measurement", unit: { id: 8594, href: "https://api.netilion.endress.com/v1/units/8594" }, timestamp: "2020-03-04T08:38:25Z", value: "96.45" } } }.to_json,
-           { "CONTENT_TYPE" => "application/json", "X-Hub-Signature-Sha256" => "" }
+           {
+             "CONTENT_TYPE" => "application/json",
+             "HTTP_X_HUB_SIGNATURE_SHA256" => "sha256=3195f5e8fe61286e95cd3db87e2b85d9c2a53d7ff3270e23de86310d74e4ae81"
+           }
     }
 
     it "returns status 200 OK" do
       expect(response.status).to eq 200
+    end
+
+    it "should write to the database.json file" do
+      expect(JSON.parse(File.read('database.json'))).to eq(
+                                                          "4079332" => {
+                                                            "time_sent" => "2020-03-04T09:15:00",
+                                                            "time_value" => "2020-03-04T08:38:25Z",
+                                                            "value" => "96.45"
+                                                          })
+    end
+  end
+
+  context "Mutliple POST to /webhook" do
+    before(:context) do
+      File.delete('database.json') if File.exist?('database.json')
+    end
+
+    let(:response_one) {
+      post "/webhook",
+           { event_type: "asset_value_created", occurred_at: "2020-03-04T09:15:00", content: { asset: { id: 4079332, href: "https://api.netilion.endress.com/v1/assets/4079332" }, value: { key: "level", group: "measurement", unit: { id: 8594, href: "https://api.netilion.endress.com/v1/units/8594" }, timestamp: "2020-03-04T08:38:25Z", value: "96.45" } } }.to_json,
+           {
+             "CONTENT_TYPE" => "application/json",
+             "HTTP_X_HUB_SIGNATURE_SHA256" => "sha256=3195f5e8fe61286e95cd3db87e2b85d9c2a53d7ff3270e23de86310d74e4ae81"
+           }
+    }
+
+    let(:response_two) {
+      post "/webhook",
+           { event_type: "asset_value_created", occurred_at: "1991-03-04T09:15:00", content: { asset: { id: 9002, href: "https://api.netilion.endress.com/v1/assets/4079332" }, value: { key: "level", group: "measurement", unit: { id: 8594, href: "https://api.netilion.endress.com/v1/units/8594" }, timestamp: "1991-03-04T08:38:25Z", value: "12.05" } } }.to_json,
+           {
+             "CONTENT_TYPE" => "application/json",
+             "HTTP_X_HUB_SIGNATURE_SHA256" => "sha256=12099265014ffcd9be16f486b790e54608d151bff63e624e0ca53e6b75b4b7c8"
+           }
+    }
+
+    it "returns status 200 OK" do
+      expect(response_one.status).to eq 200
+      expect(response_two.status).to eq 200
+    end
+
+    it "should write to the database.json file" do
+      expect(JSON.parse(File.read('database.json'))).to eq(
+                                                          "4079332" => {
+                                                            "time_sent" => "2020-03-04T09:15:00",
+                                                            "time_value" => "2020-03-04T08:38:25Z",
+                                                            "value" => "96.45"
+                                                          }, "9002" => {
+                                                          "time_sent" => "1991-03-04T09:15:00",
+                                                          "time_value" => "1991-03-04T08:38:25Z",
+                                                          "value" => "12.05"
+                                                        })
     end
   end
 end
