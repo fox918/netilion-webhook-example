1bcf00f5cd591bce9d082180b3ee636de7efc884
Oliver <oliver.wisler@endress.com>
2024-07-03 14:38:26 +0200

    feat(ops): Make it working

--- main.tf
+++ main.tf
@@ -6,26 +6,46 @@ terraform {
     }
   }
 }
+provider "heroku" {}
 
 variable "app_name" {
+  type        = string
+  default     = "netilion-example-webhook"
   description = "Name of the Heroku app to create"
+}
+
+variable "webhook_secret" {
+  type        = string
+  description = "Webhook Secret"
 }
 
 resource "heroku_app" "webhook_poc_app" {
   name   = var.app_name
   region = "eu"
+
+  organization {
+    name = "solutions-endress"
+  }
+
+  config_vars = {
+  WEBHOOK_SECRET = var.webhook_secret
+  }
+
+  buildpacks = [
+    "heroku/ruby"
+  ]
 }
 
 resource "heroku_build" "webhook_poc_build" {
-  app = heroku_app.webhook_poc_app.id
+  app_id = heroku_app.webhook_poc_app.id
 
   source {
-    path = "./app"
+    path = "app"
   }
 }
 
 resource "heroku_formation" "webhook_poc" {
-  app        = heroku_app.webhook_poc_app.id
+  app_id     = heroku_app.webhook_poc_app.id
   type       = "web"
   quantity   = 1
   size       = "Standard-1x"
@@ -33,6 +53,5 @@ resource "heroku_formation" "webhook_poc" {
 }
 
 output "app_url" {
-  value = heroku_app.webhook_poc.web_url
+  value = heroku_app.webhook_poc_app.web_url
 }
-```
